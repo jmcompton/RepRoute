f80a97302fd7bc33f772436e78683c5ec5136c63
jmcompton <jmcompton04@gmail.com>
2026-05-12 11:48:03 -0400

    fix: increase express.json body limit to 25mb for PDF quote uploads

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ const { router: brandMappingsRoutes } = require('./routes/brand_mappings');
 const quotesRoutes = require('./routes/quotes');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
